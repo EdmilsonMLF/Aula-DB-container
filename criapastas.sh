32f86a2b23e5a271c7297aece95f9418682ab5cd
EdmilsonMLF <edmilson.mlf@gmail.com>
2025-01-30 15:51:22 -0300

    pastas organizadas

--- criapastas.sh
+++ criapastas.sh
@@ -1,2 +1,9 @@
 rm -r ~/Projetos/volumes-docker/postgres-aula
-mkdir -p ~/Projetos/volumes-docker/postgres-aula/data
+mkdir -p ~/Projetos/volumes-docker/postgres-aula/data1
+mkdir -p ~/Projetos/volumes-docker/postgres-aula/data2
+mkdir -p ~/Projetos/volumes-docker/postgres-aula/data3
+mkdir -p ~/Projetos/volumes-docker/postgres-aula/data3/init-scripts
+mkdir -p ~/Projetos/volumes-docker/postgres-aula/data4
+mkdir -p ~/Projetos/volumes-docker/postgres-aula/data5
+mkdir -p ~/Projetos/volumes-docker/postgres-aula/data6
+mkdir -p ~/Projetos/volumes-docker/postgres-aula/data7
