be4cb5e9bdfeca358850584479b2e3dafed02d93
EdmilsonMLF <edmilson.mlf@gmail.com>
2025-01-30 16:01:14 -0300

    cria pastas ajustado

--- criapastas.sh
+++ criapastas.sh
@@ -1,9 +1,9 @@
-rm -r ~/Projetos/volumes-docker/postgres-aula
-mkdir -p ~/Projetos/volumes-docker/postgres-aula/data1
-mkdir -p ~/Projetos/volumes-docker/postgres-aula/data2
-mkdir -p ~/Projetos/volumes-docker/postgres-aula/data3
-mkdir -p ~/Projetos/volumes-docker/postgres-aula/data3/init-scripts
-mkdir -p ~/Projetos/volumes-docker/postgres-aula/data4
-mkdir -p ~/Projetos/volumes-docker/postgres-aula/data5
-mkdir -p ~/Projetos/volumes-docker/postgres-aula/data6
-mkdir -p ~/Projetos/volumes-docker/postgres-aula/data7
+sudo rm -r ~/Projetos/volumes-docker/
+mkdir -p ~/Projetos/volumes-docker/postgres-aula/data1/
+mkdir -p ~/Projetos/volumes-docker/postgres-aula/data2/
+mkdir -p ~/Projetos/volumes-docker/postgres-aula/data3/
+mkdir -p ~/Projetos/volumes-docker/postgres-aula/data3/init-scripts/
+mkdir -p ~/Projetos/volumes-docker/postgres-aula/data4/
+mkdir -p ~/Projetos/volumes-docker/postgres-aula/data5/
+mkdir -p ~/Projetos/volumes-docker/postgres-aula/data6/
+mkdir -p ~/Projetos/volumes-docker/postgres-aula/data7/
